46d6a26761e901c055d90046cfcce8a2e8e05fd9
UP5007 <orlandoatencio82@gmail.com>
2025-12-01 11:31:39 -0500

    arrglo de repositorio

--- giras_tecnicas/api/logout.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-require_once __DIR__ . '/../config/session.php';
-require_once __DIR__ . '/../utils/response.php';
-
-Session::destroy();
-Response::success('Sesión cerrada exitosamente');
-?>
